bb907fe3862d889701cde0b7142e8e78264a52fe
Andrew Bruce <andrew@camelpunch.com>
2014-06-23 00:39:15 -0700

    tools.namespace set up

--- project.clj
+++ project.clj
@@ -7,10 +7,12 @@
                  [ring/ring-jetty-adapter "1.3.0"]]
   :uberjar-name "andrewbruce-standalone.jar"
   :min-lein-version "2.0.0"
-  :main net.andrewbruce
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}
-             :dev {:plugins [[lein-ring "0.8.11"]]
+  :profiles {:uberjar {:aot :all
+                       :main net.andrewbruce}
+             :dev {:source-paths ["dev"]
+                   :dependencies [[org.clojure/tools.namespace "0.2.4"]]
+                   :plugins [[lein-ring "0.8.11"]]
                    :ring {:handler net.andrewbruce/app
                           :auto-reload? true
                           :auto-refresh? true}}})
